70fb3741ade1eab475301abd626c9c51c37307d7
David <avatar_4500@mail.ru>
2020-12-11 21:21:29 +0300

    more files

---
+++ cmac.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from neupy import algorithms, utils
+
+
+utils.reproducible()
+plt.style.use('ggplot')
+
+X_train = np.reshape(np.linspace(0, 2 * np.pi, 100), (100, 1))
+X_test = np.reshape(np.sort(2 * np.pi * np.random.random(50)), (50, 1))
+
+y_train = np.sin(X_train)
+y_test = np.sin(X_test)
+
+cmac = algorithms.CMAC(
+    quantization=100,
+    associative_unit_size=10,
+    step=0.1,
+    verbose=True,
+    show_epoch=10,
+)
+cmac.train(X_train, y_train, epochs=100)
+predicted_test = cmac.predict(X_test)
+
+plt.plot(X_train, y_train)
+plt.plot(X_test, predicted_test)
+plt.show()
---
+++ f1_ROC_heartCateg.py
@@ -0,0 +1,73 @@
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+import pandas as pd
+
+import tensorflow as tf
+from keras import backend as K
+
+
+def f1(y_true, y_pred):
+    def recall(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+    def precision(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
+
+
+def roc_auc_score(y_true, y_pred):
+    """ ROC AUC Score.
+    Approximates the Area Under Curve score, using approximation based on
+    the Wilcoxon-Mann-Whitney U statistic.
+    Yan, L., Dodier, R., Mozer, M. C., & Wolniewicz, R. (2003).
+    Optimizing Classifier Performance via an Approximation to the Wilcoxon-Mann-Whitney Statistic.
+    Measures overall performance for a full range of threshold levels.
+    Arguments:
+        y_pred: `Tensor`. Predicted values.
+        y_true: `Tensor` . Targets (labels), a probability distribution.
+    """
+    with tf.name_scope("RocAucScore"):
+        pos = tf.boolean_mask(y_pred, tf.cast(y_true, tf.bool))
+        neg = tf.boolean_mask(y_pred, ~tf.cast(y_true, tf.bool))
+        pos = tf.expand_dims(pos, 0)
+        neg = tf.expand_dims(neg, 1)
+        # original paper suggests performance is robust to exact parameter choice
+        gamma = 0.2
+        p = 3
+        difference = tf.zeros_like(pos * neg) + pos - neg - gamma
+        masked = tf.boolean_mask(difference, difference < 0.0)
+    return tf.reduce_sum(tf.pow(-masked, p))
+
+
+data = pd.read_csv("categ.csv", header=0)
+
+X = data.drop('target', axis=1)
+Y = data['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=39, shuffle=True)
+
+scaler = MinMaxScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_train = pd.DataFrame(X_train_scaled)
+X_test_scaled = scaler.fit_transform(X_test)
+X_test = pd.DataFrame(X_test_scaled)
+y_train = y_train
+y_test = y_test
+reducelrp = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=30, verbose=1, mode='auto', min_delta=0.0001,
+                              cooldown=0, min_lr=0)
+i = 0
+#for i in range(len(y_train)):
+#    print(y_train(i))
+print(y_train)
